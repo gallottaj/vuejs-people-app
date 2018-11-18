e4a27e4fd23d91f5f6fec8f01dbd82097576777c
Joe Gallotta <joeygallotta@gmail.com>
2018-11-18 11:36:10 -0600

    use v-for to display a list of people

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <ol>
+        <li v-for="people in peoples">name: {{ people.name }} bio: {{ people.bio }} bioVisible: {{ people.bioVisible }}</li>
+    </ol>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      peoples: [
+        {name: "tom" , bio: "all about tom", bioVisible: "true"},
+        {name: "dan" , bio: "all about dan", bioVisible: "true"}
+      ]
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+</script>
+
+
+
