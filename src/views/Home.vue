1406389184dc6b0e9d1e2da718169da1fd974e30
Joe Gallotta <joeygallotta@gmail.com>
2018-11-18 11:49:56 -0600

    add form and button to add new people

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <ol>
-        <li v-for="people in peoples">name: {{ people.name }} bio: {{ people.bio }} bioVisible: {{ people.bioVisible }}</li>
-    </ol>
+    <ul>
+        <li v-for="person in people">name: {{ person.name }} bio: {{ person.bio }} bioVisible: {{ person.bioVisible }}</li>
+    </ul>
+    <p>name:<input type="text" v-model="newPerson.name"></p>
+    <p>bio:<input type="text" v-model="newPerson.bio"></p>
+    <p>bioVisible:<input type="text" v-model="newPerson.bioVisible"></p>
+    <p><button v-on:click="addPerson()">Click to add a person</button></p>
   </div>
 </template>
 
@@ -15,14 +19,21 @@ export default {
   data: function() {
     return {
       message: "Welcome to Vue.js!",
-      peoples: [
+      people: [
         {name: "tom" , bio: "all about tom", bioVisible: "true"},
         {name: "dan" , bio: "all about dan", bioVisible: "true"}
-      ]
+      ],
+      newPerson: {name: "" , bio: "", bioVisible: "true"}
     };
   },
   created: function() {},
-  methods: {},
+  methods: {
+    addPerson: function() {
+      console.log('adding the person...');
+      var newPerson = {name: "" , bio: "", bioVisible: "true"};
+      this.people.push(newPerson);
+    }
+  },
   computed: {}
 };
 </script>
